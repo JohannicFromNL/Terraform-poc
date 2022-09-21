83101cf2c8af7baa9e327864cdf4972e33c1f733
JohannicFromNL <114067765+JohannicFromNL@users.noreply.github.com>
2022-09-21 12:10:24 +0200

    Update main2.tf

--- terraform/main2.tf
+++ terraform/main2.tf
@@ -1,33 +1,4 @@
 
-
-Skip to content
-Pull requests
-Issues
-Marketplace
-Explore
-@JohannicFromNL
-JohannicFromNL /
-Terraform-poc
-Public
-
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-
-    Settings
-
-Terraform-poc/main.tf
-@JohannicFromNL
-JohannicFromNL Update main.tf
-Latest commit dfefcd3 9 minutes ago
-History
-1 contributor
-35 lines (29 sloc) 750 Bytes
 terraform {
  required_version = "1.2.3" 
  required_providers {
@@ -63,19 +34,4 @@ Output "id" {
  value=data.azurerm_resource_group.rg010.id
 }
 
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-
-    Terms
-    Privacy
-    Security
-    Status
-    Docs
-    Contact GitHub
-    Pricing
-    API
-    Training
-    Blog
-    About
 
